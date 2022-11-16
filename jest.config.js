849583804d86ba6dc802a68b2e4a539f26170cba
CharaD7 <jijakahn6@gmail.com>
2022-11-16 11:56:26 +0000

    Refactored Jest setupFilesEnv name

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,5 @@
 module.exports = {
   moduleDirectories: ['.', 'node_modules'],
   modulePaths: ['<rootDir>'],
-  setupFilesAfterEnvironment: ['<rootDir>/jest.setup.js']
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js']
 };
